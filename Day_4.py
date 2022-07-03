dd03e640b7d640d96b274ecf12afe8d0cf1e509e
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-04 04:11:21 +0500

    Day 4 last project complete rock paper scissors

--- Day_4.py
+++ Day_4.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -26,5 +27,28 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+game_images = [rock,paper,scissors]
+user = int(input("What do you choose?Type 0 for Rock,1 for paper or 2 for Scissors\n"))
+if user >= 3 or user < 0:
+    print("You typed an invalid number you lose!")
+else:
+    print("You Choose")
+    print(game_images[user - 1])
+    computer = random.randint(0,2)
+    print("Computer Choose")
+    print(game_images[computer - 1])
 
-print(paper)
+    if user == 1 and computer == 2:
+        print("You Lose!")
+    elif user == 2 and computer == 0:
+        print("You Lose!")
+    elif user == 0 and computer == 1:
+        print("You Lose!")
+    elif user == 0 and computer == 2:
+        print("You Win!")
+    elif user == 1 and computer == 2:
+        print("You Lose!")
+    elif user == 2 and computer == 1:
+        print("You Lose!")
+    elif user == computer:
+        print("Its a Draw")         
