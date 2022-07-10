edac19ef98bfd13add9d1f5214b0452b62183251
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-10 05:34:54 +0500

    Day_10 is start dynamic calculator done 90%

--- Day_10.py
+++ Day_10.py
@@ -26,15 +26,41 @@ operations = {
   "/" : divide 
    }
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+def calculator():
+    num1 = int(input("Enter the a number: "))
+    for operators in operations:
+        print(operators)   
 
-for operators in operations:
-    print(operators)
+    want_to_continue = True  
+    while want_to_continue:
+        which_operations = input("Pick up an operation: ")
+        num2 = int(input("What the next number: "))
+        calculation_function = operations[which_operations]
+        answer = calculation_function(num1,num2)
+        print(f"{num1} {which_operations} {num2} = {answer}")
+        if input(f"Type y to continue calculating with {answer} and n to start agains ") == "y":
+            num1 = answer 
+        else:
+            want_to_continue = False
+            calculator()
+            
+calculator()
+  
 
-which_operations = input("Pick an operation from the line above: ")
+       
+
+
+
+
+
+
+
+
+
+
+
+
 # fisrt solution
-
 
 # if which_operations == "+":
 #     answer = add(num1,num2)
@@ -46,10 +72,3 @@ which_operations = input("Pick an operation from the line above: ")
 #     answer = divide(num1,num2)          
 
 
-# second solution
-
-calculation_function = operations[which_operations]
-answer = calculation_function(num1,num2)
-
-
-print(f"{num1} {which_operations} {num2} = {answer}")
