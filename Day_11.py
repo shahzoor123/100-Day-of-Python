6d2d44c11fbe289a967a6a9184f363091233b806
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-11 22:47:25 +0500

    capstone project of Day 11 black jack game i have to work on my version

--- Day_11.py
+++ Day_11.py
@@ -18,51 +18,83 @@
 ## The computer is the dealer.
 
 ##################### Hints #####################
-from sys import flags
-from tkinter import E
 import mod.blackjack_art as art
 import os
 import random
 
 def deal():
-    for i in range(2):
-        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        card = random.choice(cards)
-        return card
-        
+    
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)    
+    return sum(cards)
+            
+
+user_cards  = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal())
+    computer_cards.append(deal())
+
+while not is_game_over:
+
+    user_score = calculate_score(user_cards)            
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}, current score: {user_score}")
+    print(f"Computer's first card: {computer_cards[0]}")         
+    if user_score == 0 and computer_score == 0 or user_score > 21:
+        is_game_over = True               
+    else:
+        want_to_stand_or_hit = input(f"Type 'y' to get another card, type 'n' to pass: y ")
+        if want_to_stand_or_hit == "y":
+            user_cards.append(deal())
+        else:
+            is_game_over = True    
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal())
+    computer_score = calculate_score(computer_cards)
+
+
+
+
+
+
+# def black_jack():
+#     card_limit = 21
+#     flag1 = 0
+#     empty_list = []
+#     while flag1 < 2:
+#         flag1 += 1
+#         data = deal() 
+#         def add_to_list(num):
+#             empty_list.append(num)
+#             full_list = []
+#             full_list += empty_list
+            
+#             return full_list
+#         def sum_of_list():
+#             sum_of_list = 0
+#             list1 = add_to_list(data)
+#             for nums in list1:
+#                 sum_of_list += nums
+#             return sum_of_list
+#         score = sum_of_list()
+#         lists = add_to_list(data)
+#         print(f"Your cards: {lists}, current score: {score}")
    
-def black_jack():
-    card_limit = 21
-    flag1 = 0
-    empty_list = []
-    while flag1 < 3:
-        flag1 += 1
-        data = deal() 
-        def add_to_list(num):
-            empty_list.append(num)
-            full_list = []
-            full_list += empty_list
-            
-            return full_list
-        def sum_of_list():
-            sum_of_list = 0
-            list1 = add_to_list(data)
-            for nums in list1:
-                sum_of_list += nums
-            return sum_of_list
-        score = sum_of_list()
-        lists = add_to_list(data)
-        print(f"Your cards: {lists}, current score: {score}")
-        # want_to_play = input(f"Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-        # if want_to_play == "y":
-        #     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        #     first_round = random.choice(cards)
-            
-        # else:
-        #     os.system("cls")
-
-
-black_jack()
+# black_jack()
 
 
 # want_to_stand_or_hit = input(f"Type 'y' to get another card, type 'n' to pass: y ")
