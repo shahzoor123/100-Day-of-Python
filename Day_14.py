a907087664e2469195659f0445d6bf1d78b8556b
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-13 20:48:38 +0500

    Day_14 Project of higher lower game score tracking and b == a function is remainging

--- Day_14.py
+++ Day_14.py
@@ -1,41 +1,55 @@
+from pydoc import describe
+from typing import final
 import mod.higher_lower_art as art
 import random
+import os
 import mod.game_data_of_higher_lower as list_dict
 print(art.logo)
 
+game_loop = True
+while game_loop:
+    """one for to compare reveal data one to be guessed by the user not reveal the data"""
 
-def compare(compare_a,agiast_b,user_guess):
-    
+    def compare(compare_a,against_b):
+        print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]} , {compare_a["follower_count"]}')
+        print(art.vs)
+        print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}, {against_b["follower_count"]}')
+        score = 0
+        a = compare_a['follower_count']
+        b = against_b['follower_count']
+        user_guess = input("Who has more followers? Type 'A' or 'B':") 
+        g = user_guess
+        """Guess again loop"""
+        
+            
+        if g == "a" and a > b:
+                
+            """"Generate a two random keys from the list"""
+            print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]}')
+            print(art.vs)
+            print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}')
+
+            score += 1
+            """if user guess is == right than the user guessed value became a and b == new value """
+            new_guess = compare_a
+            compare_a = against_b
+            against_b = random.choice(data)
+            """show the score of the user of how many guesses"""
+            return print(f"You're right! Current score: {score}.") 
+        else:
+            global game_loop 
+            game_loop = False
+            """if not guess right game is over"""
+            os.system('cls')
+                
+            return print(f"Sorry, that's wrong. Final score: {score}") 
 
 
 
 
-
-
-data = list_dict.data
-""""Generate a two random keys from the list"""
-for _ in range(1):
-
+    data = list_dict.data
     compare_a = random.choice(data)
-    print(compare_a)
-
-    print(art.vs)
-
     against_b = random.choice(data)
-    print(against_b)
 
-
-"""one for to compare reveal data one to be guessed by the user not reveal the data"""
-
-
-"""Ask user a input to guess"""
-
-"""if user guess is == right than the user guessed value became a and b == new value """
-
-
-"""Guess again loop"""
-
-"""if not guess right game is over"""
-
-
-"""show the score of the user of how many guesses"""
+                
+    compare(compare_a,against_b)            
--- Thonny/dealspy.py
+++ Thonny/dealspy.py
@@ -1,3 +1,5 @@
+import random
+import os
 data = [
     {
         'name': 'Instagram',
@@ -17,6 +19,58 @@ data = [
         'description': 'Musician and actress',
         'country': 'United States'
     },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
     ]
-x = data[0]['follower_count']
-print(x)
+def compare(compare_a,against_b):
+    print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]} , {compare_a["follower_count"]}')
+  
+    print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}, {against_b["follower_count"]}')
+    score = 0
+    a = compare_a['follower_count']
+    b = against_b['follower_count']
+    user_guess = input("Who has more followers? Type 'A' or 'B':") 
+    g = user_guess
+    """Guess again loop"""
+    game_loop = True
+    while game_loop == True:
+        
+        if g == "a" and a > b:
+            
+            """"Generate a two random keys from the list"""
+            print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]}')
+            
+            print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}')
+
+            score += 1
+            """if user guess is == right than the user guessed value became a and b == new value """
+            new_guess = compare_a
+            compare_a = against_b
+            against_b = random.choice(data)
+            """show the score of the user of how many guesses"""
+            return print(f"You're right! Current score: {score}.") 
+        else:
+            """if not guess right game is over"""
+            os.system('cls')
+            
+            return print(f"Sorry, that's wrong. Final score: {score}") 
+
+
+
+
+
+compare_a = random.choice(data)
+against_b = random.choice(data)
+
+            
+compare(compare_a,against_b)            
