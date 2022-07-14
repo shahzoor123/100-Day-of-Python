dda390174d7454f2fb854ee3a2afc84f75197301
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-14 13:48:15 +0500

    Day 15 coffee making machine

--- Day_14.py
+++ Day_14.py
@@ -5,36 +5,43 @@ import random
 import os
 import mod.game_data_of_higher_lower as list_dict
 print(art.logo)
+data = list_dict.data
 
 game_loop = True
 score = 0
-
+against_b = random.choice(data)   
 while game_loop:
+    
     os.system('cls')
-    score = score + 1
+    compare_a = against_b
+    against_b = random.choice(data)
+    score += 1
+
     def Higher_lower(compare_a,against_b):
-        print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]} , {compare_a["follower_count"]}')
+        print(f"You're right! Current score: {score}.")
+        print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]}')
         print(art.vs)
-        print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}, {against_b["follower_count"]}')
-        
+        print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}')
+      
         a = compare_a['follower_count']
         b = against_b['follower_count']
         user_guess = input("Who has more followers? Type 'A' or 'B':") 
         g = user_guess
+        
         
         if g == "a" and a >= b:
             """"Generate a two random keys from the list"""
             print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]}')
             print(art.vs)
             print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}')
-            return print(f"You're right! Current score: {score}.")
+            
 
         elif g == "b" and b >= a:
             """"Generate a two random keys from the list"""
             print(f'Compare A: {compare_a["name"]} , {compare_a["description"]} , {compare_a["country"]}')
             print(art.vs)
             print(f'Against B: {against_b["name"]} , {against_b["description"]} , {against_b["country"]}')
-            return print(f"You're right! Current score: {score}.")
+            
 
         else:
             os.system('cls')
@@ -42,7 +49,7 @@ while game_loop:
             game_loop = False
             return print(f"Sorry, that's wrong. Final score: {score}") 
 
-    data = list_dict.data
-    compare_a = random.choice(data)
-    against_b = random.choice(data)   
+    
+
+    
     Higher_lower(compare_a,against_b)            
