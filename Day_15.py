792dc1b50bec2139c93d48e8aa708057cd31445c
shahzor khan <shahzoorkhan321@gmail.com>
2022-07-14 22:55:23 -0700

    Coffee machine report

--- Day_15.py
+++ Day_15.py
@@ -41,16 +41,16 @@ def report(money, resources_used):
     return print(f"water:{resources['water']}\nMilk:{resources['milk']}\nCoffee:{resources['coffee']}")
 
 
-machine_off = True
-while machine_off:
-    user_input = input("“What would you like? (espresso/latte/cappuccino): ")
-    if user_input == "off":
-        machine_off = False
-    elif user_input == "report":
-        print(report())
-    elif user_input == "espresso":
-        pass
-    elif user_input == "latter":
-        pass
-    elif user_input == "cappuccino":
-        pass
+# machine_off = True
+# while machine_off:
+#     user_input = input("“What would you like? (espresso/latte/cappuccino): ")
+#     if user_input == "off":
+#         machine_off = False
+#     elif user_input == "report":
+#         print(report())
+#     elif user_input == "espresso":
+#         pass
+#     elif user_input == "latter":
+#         pass
+#     elif user_input == "cappuccino":
+#         pass
