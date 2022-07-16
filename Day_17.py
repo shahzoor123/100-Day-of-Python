aeb739267c906d2cc8e406ceb1410dace333371d
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-16 20:49:11 +0500

    Day_17 oops quiz game

---
+++ Day_17.py
@@ -0,0 +1,14 @@
+from mod.quiz.data import question_data
+from mod.quiz.question_model import Questions
+
+question_bank = []
+data = question_data
+for questions in data:
+    questions_text = questions['text']
+    questions_answer = questions['answer']
+    new_questions = Questions(questions_text, questions_answer)
+    question_bank.append(new_questions)
+print(question_bank)
+
+
+
