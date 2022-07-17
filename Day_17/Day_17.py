70451c19b980da7f2225ed87579445e715660820
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-17 23:05:24 +0500

    Quiz brain complete having litte difficulty but trying

--- Day_17/Day_17.py
+++ Day_17/Day_17.py
@@ -1,3 +1,19 @@
+from quiz.data import question_data
+from quiz.question_model import Questions
+from quiz.quiz_brain import QuizBrain
+
+
+question_bank = []
+data = question_data
+for questions in data:
+    questions_text = questions['text']
+    questions_answer = questions['answer']
+    new_questions = Questions(questions_text, questions_answer)
+    question_bank.append(new_questions)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
 
 
 
