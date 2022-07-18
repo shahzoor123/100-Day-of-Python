5938866161aba1b42b06def165d44a0e154ba888
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-19 04:22:17 +0500

    Day 19 2 challage turtles racing game woring on every turtle indivisualy

--- Day_19/Day_19_2.py
+++ Day_19/Day_19_2.py
@@ -2,9 +2,15 @@ import turtle
 import random
 from turtle import Turtle, Screen
 
+is_race_on = False
 turtle.colormode(255)
 y_positions = [10, 50, 90, 130, 170, -50]
 colors = ['blue', 'red', 'pink', 'green', 'brown', 'yellow']
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+all_turtles = []
+
 
 for num in range(0, 5):
     tim = Turtle(shape='turtle')
@@ -12,8 +18,13 @@ for num in range(0, 5):
     tim.goto(x=-230, y=y_positions[num])
     tim.color(colors[num])
 
-screen = Screen()
-screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    random_distance = random.randint(0, 10)
+    turtle.forward(random_distance)
+
+
 screen.exitonclick()
