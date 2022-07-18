455a71eccee7894604d5c96d20e53f35f74a33fe
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-18 18:52:55 +0500

    turtle 5 challage spirograph

--- Day_18/Day_18_2.py
+++ Day_18/Day_18_2.py
@@ -1,20 +1,28 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
+turtle.colormode(255)
 tummy = Turtle()
 tummy.pensize(width=5)
 tummy.speed(500)
-directions = [0, ]
+directions = [0, 90, 180, 270]
 
 tummy.shape('turtle')
+
+
 # colors = ['gray', 'royal blue', 'medium turquoise', 'medium sea green', 'tan', 'light salmon',
 #           'rosy brown', 'dark slate blue', 'lavender']
 
-colors = ['gray', 'black']
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
 
 for _ in range(1000):
-    tummy.color(random.choice(colors))
-    tummy.forward(random.randint(1, 40))
-    tummy.right(random.randint(1, 200))
-
-
+    tummy.color(random_color())
+    tummy.forward(random.randint(1, 30))
+    tummy.setheading(random.choice(directions))
