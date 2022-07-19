346f42a0a4799da6908276554c0c5ab3515196b4
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-19 18:34:14 +0500

    Day 20 snake snake creating snake class

--- Day_20/Day_20_snake_game.py
+++ Day_20/Day_20_snake_game.py
@@ -21,10 +21,13 @@ for turtles in range(3):
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
     for seg_num in range(len(segments) - 1, 0, -1):
         new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num].ycor()
+        new_y = segments[seg_num - 1].ycor()
         segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
+
 
 screen.exitonclick()
---
+++ Day_20/snake_class.py
@@ -0,0 +1,9 @@
+class Snake:
+
+    def __init__(self):
+        pass
+
+    def snake(self):
+        pass
+
+    def snake_move(self):
