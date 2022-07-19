9ae9da93e114c6f7774eca8869ddf0ae05bc7cc4
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-19 20:11:34 +0500

    Day 20 snake snake creating snake class and snake class for control also done

--- Day_20/snake_class.py
+++ Day_20/snake_class.py
@@ -9,6 +9,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
 
@@ -24,16 +25,16 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.segments[0].setheading(90)
+        self.head.setheading(90)
 
     def down(self):
-        self.segments[0].setheading(270)
+        self.head.setheading(270)
 
     def left(self):
-        self.segments[0].setheading(180)
+        self.head.setheading(180)
 
     def right(self):
-        self.segments[0].setheading(0)
+        self.head.setheading(0)
