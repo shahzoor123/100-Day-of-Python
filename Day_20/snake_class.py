a77cc110fc1a47114cd6d760da6ed48a1fcc528d
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-19 20:28:11 +0500

    Day 20 snake snake creating snake class and snake class for control also done

--- Day_20/snake_class.py
+++ Day_20/snake_class.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 X_POSITIONS = [-20, -40, -60]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -28,13 +32,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != LEFT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(RIGHT)
