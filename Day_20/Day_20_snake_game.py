6fb2df2efa50e3560f6cc6dac67bddc7537c211d
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-19 20:03:47 +0500

    Day 20 snake snake creating snake class and snake class for control also done

--- Day_20/Day_20_snake_game.py
+++ Day_20/Day_20_snake_game.py
@@ -1,33 +1,27 @@
 import random
 from turtle import Turtle, Screen
 import time
+from snake_class import Snake
 
-x_positions = [-20, -40, -60]
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake game')
 screen.tracer(0)
+screen.listen()
 
-segments = []
-for turtles in range(3):
-    new_turtle = Turtle()
-    new_turtle.penup()
-    new_turtle.shape('square')
-    new_turtle.color('white')
-    new_turtle.goto(x_positions[turtles], y=0)
-    segments.append(new_turtle)
+snake = Snake()
+
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    time.sleep(.1)
 
+    snake.snake_move()
 
 screen.exitonclick()
--- Day_20/snake_class.py
+++ Day_20/snake_class.py
@@ -1,9 +1,39 @@
+from turtle import Turtle
+
+X_POSITIONS = [-20, -40, -60]
+MOVE_DISTANCE = 20
+
+
 class Snake:
 
     def __init__(self):
-        pass
+        self.segments = []
+        self.create_snake()
 
-    def snake(self):
-        pass
+    def create_snake(self):
+
+        for turtles in range(3):
+            new_turtle = Turtle('square')
+            new_turtle.penup()
+            new_turtle.color('white')
+            new_turtle.goto(X_POSITIONS[turtles], y=0)
+            self.segments.append(new_turtle)
 
     def snake_move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def left(self):
+        self.segments[0].setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
