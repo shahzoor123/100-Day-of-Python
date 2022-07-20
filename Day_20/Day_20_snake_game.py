6fa693ed1b4549de023a9e3a164fd9063d967558
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-20 19:10:06 +0500

    21 day snake game final day creating wall hit game over function done

--- Day_20/Day_20_snake_game.py
+++ Day_20/Day_20_snake_game.py
@@ -31,4 +31,7 @@ while game_is_on:
         food.food_refresh()
         score.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
 screen.exitonclick()
