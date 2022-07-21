b0a8d75e7adea4e02c782f62957699aaa52f11e4
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-21 06:42:49 +0500

    Day 22 Pong Game paddle class done

--- Day_22/Day_22.py
+++ Day_22/Day_22.py
@@ -1,36 +1,22 @@
 from turtle import Turtle, Screen
-
+from paddal_class import Paddle
 screen = Screen()
 screen.title('Pong Game')
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.tracer(0)
 
-paddle1 = Turtle()
-paddle1.shape('square')
-paddle1.color('white')
-paddle1.penup()
-paddle1.turtlesize(5, 1)
-paddle1.goto(350, 0)
-
-
-def up():
-    new_y = paddle1.ycor() + 40
-    paddle1.goto(paddle1.xcor(), new_y)
-
-
-def down():
-    new_y = paddle1.ycor() - 40
-    paddle1.goto(paddle1.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(up, 'Up')
-screen.onkey(down, 'Down')
+screen.onkey(r_paddle.up, 'Up')
+screen.onkey(r_paddle.down, 'Down')
+screen.onkey(l_paddle.up, 'w')
+screen.onkey(l_paddle.down, 's')
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
 
 screen.exitonclick()
