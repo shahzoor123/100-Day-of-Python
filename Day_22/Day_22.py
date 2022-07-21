67ef102bef531ff0631865f2fd12ba071e61a0a6
shahzoorkhan <lamborghinia693@gmail.com>
2022-07-21 08:34:40 +0500

    Day 22 Pong Game paddle class done ball class is also done

--- Day_22/Day_22.py
+++ Day_22/Day_22.py
@@ -1,5 +1,8 @@
 from turtle import Turtle, Screen
 from paddal_class import Paddle
+from ball_class import Ball
+import time
+
 screen = Screen()
 screen.title('Pong Game')
 screen.setup(width=800, height=600)
@@ -8,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.up, 'Up')
@@ -17,6 +21,16 @@ screen.onkey(l_paddle.down, 's')
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.ball_movement()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 100 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    if ball.xcor() > 380:
+        ball.reset()
+    if ball.xcor() < -380:
+        ball.reset()
 
 screen.exitonclick()
